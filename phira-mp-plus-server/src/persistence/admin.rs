f5de335d5bb405627512b0462f68aaf460abbc81
FireflyF09 <nb3502022@outlook.com>
2026-07-23 15:30:56 +0800

    fix: admin.rs semicolon (Option return)

--- phira-mp-plus-server/src/persistence/admin.rs
+++ phira-mp-plus-server/src/persistence/admin.rs
@@ -36,6 +36,6 @@ impl DbManager {
         .await
         .ok()??;
         let raw = row.try_get::<String, _>("value").ok()?;
-        serde_json::from_str(&raw).ok();
+        serde_json::from_str(&raw).ok()
     }
 }
